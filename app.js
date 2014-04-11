97705c8f836ed933523b5be59b53864dcf23086c
Summer <summermichiko@gmail.com>
2014-04-10 23:40:39 -0400

    everything before newly added items can be removed and checked

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 $(document).ready(function() {
-	$(".top-white-line").click(".bird-img", function() {
+	$(".bird-img").click(function() {
 		$(this).closest(".top-white-line").find(".bird-talk").toggle();
 	});
-	$("#removeItem").click(function() {
+		$("#addItem").click(function(e) {
+		e.preventDefault();
+		var newItem = $("#make-new-item-box").val();
+		$(this).closest("body").find(".list-item-section").prepend('<ul class="white-rec"><img src="file:///Users/summermcdonald/Desktop/Project/Shopping%20List%20App/images/images.jpg" class="white-square"><img src="images/imgres.jpg" class="check-mark"><li class="thing">' + newItem + '</li><button type="remove" class="removeItem">Remove</button></ul>').slideDown();
+		$(function(){
+    	$("#make-new-item-box").val('');
+		});
+	});
+	$(".removeItem").click(function() {
 		$(this).closest(".white-rec").slideUp();
 	});
 	$(".white-square").click(function() {
-		$(this).closest(".white-rec").find(".check-mark").toggle();
+		$(this).closest(".white-rec").find(".check-mark").show();
+	});
+	$(".check-mark").click(function() {
+		$(this).hide();
 	});
 });
-
-/*$(document).keydown(function(event) {
-		if (event.which == 88) {
-			$('.ryu-still').hide();
-			$('.ryu.ready').hide();
-			$('.ryu-cool').show();
-		}
-	})
 
 
